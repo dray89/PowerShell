3d79f12c9903374afa4a49678f19524852754d69
dray89 <46060603+dray89@users.noreply.github.com>
2019-02-20 22:09:41 -0500

    Add files via upload

---
+++ cleandownloads.ps1
@@ -0,0 +1,6 @@
+
+Move-Item *.dta C:\Users\faith\iCloudDrive\School\StataDataSets
+Move-Item *.pdf C:\Users\faith\iCloudDrive\School\PDFFiles
+Move-Item *.pptx C:\Users\faith\iCloudDrive\School\PPTFiles
+Move-Item *.do C:\Users\faith\iCloudDrive\School\Dofiles
+Move-Item *.xls* C:\Users\faith\iCloudDrive\ExcelFiles
