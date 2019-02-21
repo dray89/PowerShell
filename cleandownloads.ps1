2b46205ad083d4e020425e1cc536be50e54666d9
dray89 <46060603+dray89@users.noreply.github.com>
2019-02-20 22:40:27 -0500

    Update cleandownloads.ps1

--- cleandownloads.ps1
+++ cleandownloads.ps1
@@ -1,6 +1,6 @@
 
-Move-Item *.dta C:\Users\faith\iCloudDrive\School\StataDataSets
-Move-Item *.pdf C:\Users\faith\iCloudDrive\School\PDFFiles
-Move-Item *.pptx C:\Users\faith\iCloudDrive\School\PPTFiles
-Move-Item *.do C:\Users\faith\iCloudDrive\School\Dofiles
-Move-Item *.xls* C:\Users\faith\iCloudDrive\ExcelFiles
+Move-Item *.dta $Path\StataDataSets
+Move-Item *.pdf $Path\PDFFiles
+Move-Item *.pptx $Path\PPTFiles
+Move-Item *.do $Path\Dofiles
+Move-Item *.xls* $Path\ExcelFiles
